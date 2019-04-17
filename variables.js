c02850e905a34151666a152d729935b302549cf0
Rachel Rene Chan <49720489+rachelrenechan@users.noreply.github.com>
2019-04-17 09:22:26 -0400

    My notes on how to use variables in javascript

---
+++ variables.js
@@ -0,0 +1,27 @@
+
+// Variables 
+
+// Initialize variables
+
+
+// using var (undefined, for conditionals)
+var greeting; 
+
+// using let
+let greeting;
+
+// using const
+const name = 'Rachel';
+console.log(name); // prints Rachel in console.log
+// name = 'Sara'; 
+// console.log(name); // error in console log as const var cannot be reassigned
+
+// using const with objects
+const person = {
+    name: 'John',
+    age: 30
+}
+person.name = 'Sara';
+person.age = 32;
+console.log(person.name);
+console.log(person.age); //chg data in obj is ok. mutatable 
