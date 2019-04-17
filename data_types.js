98ab66384627fedb6e1b0c1cb07d25dc74af2cc7
Rachel Rene Chan <49720489+rachelrenechan@users.noreply.github.com>
2019-04-17 09:29:30 -0400

    Notes on data types

---
+++ data_types.js
@@ -0,0 +1,20 @@
+// Data Types
+
+// Primitive Data Types
+// string, number, boolean, null, undefined, symbols(es6)
+
+// Reference Data Types (objects)
+// Arrays, Object literals, function, dates, anything else
+
+// Check Data Types using typeof
+const name = 'John';
+const age = 45;
+console.log(typeof name); //string
+console.log(typeof age); //number
+const hobbies = ['movies', 'music'];
+const address = {
+    city:'Boston',
+    state: 'MA'
+}
+console.log(typeof hobbies); // object
+console.log(typeof address); //object
